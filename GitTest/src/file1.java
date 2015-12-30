9c2d53db0aa874fdb4f87def25f029f5be61501d
abdessamad mahassine <abdessamad 7mahassine@gmail.com>
2015-12-30 21:11:39 +0000

    organisation supprimer les espces

--- GitTest/src/file1.java
+++ GitTest/src/file1.java
@@ -1,14 +1,14 @@
 
 public class file1 {
-	public static void addition (int a,int b){
+	public static void addition (int a,int b)
+	{
 		int S;
 		S=a+b;
 		System.out.println(S);
-		
 	}
 	
-	public static void main (String arg[]){
-		
+	public static void main (String arg[])
+	{
 		System.out.println("La somme de 5+7");
 		addition(5,7);
 	}
