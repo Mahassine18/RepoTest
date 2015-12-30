ddb209e2b7d6fdef2e1242d1f88e621b9e079220
abdessamad mahassine <abdessamad 7mahassine@gmail.com>
2015-12-30 20:14:32 +0000

    ajouter la fonction addition

--- GitTest/src/file1.java
+++ GitTest/src/file1.java
@@ -1,9 +1,14 @@
 
 public class file1 {
+	public static void addition (int a,int b){
+		int S;
+		S=a+b;
+		System.out.println(S);
+		
+	}
+	
 	public static void main (String arg[]){
 		
-	
-	
 
 	}
 }
